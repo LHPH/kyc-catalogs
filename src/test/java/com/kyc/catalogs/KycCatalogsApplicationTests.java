3be5860b7b423aaf46bd9beb36c51e43c87c0e2c
Luis H <luislyly@hotmail.com>
2020-04-13 14:51:41 -0500

    refactor: Add project, add files and add sql

---
+++ src/test/java/com/kyc/catalogs/KycCatalogsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kyc.catalogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KycCatalogsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
