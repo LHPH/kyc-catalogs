39d47106eaf890c083d7a8cd630b12010e61fb97
LHPH <luislyly@hotmail.com>
2024-05-29 21:19:15 -0600

    Update paths

--- src/main/java/com/kyc/catalogs/constants/AppConstants.java
+++ src/main/java/com/kyc/catalogs/constants/AppConstants.java
@@ -2,9 +2,9 @@ package com.kyc.catalogs.constants;
 
 public class AppConstants {
 
-	public static final String ENDPOINT_CATALOG_INDEX = "/catalogs";
-	public static final String ENDPOINT_CATALOG="/catalogs/{catalog}";
-	public static final String ENDPOINT_CATALOG_CRITERIA="/catalogs/single/{catalog}/{criteria}";
+	public static final String ENDPOINT_CATALOG_INDEX = "/";
+	public static final String ENDPOINT_CATALOG="/{catalog}";
+	public static final String ENDPOINT_CATALOG_CRITERIA="/single/{catalog}/{criteria}";
 	public static final String ENDPOINT_CATALOG_CLEAN_CACHE = "/cache/eviction";
 
 	public static final String MESSAGE_001 = "001";
